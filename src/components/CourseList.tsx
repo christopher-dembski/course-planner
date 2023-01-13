2f90875cc26723f7637e2db63e5847ff4060aac6
christopher-dembski <christopher_dembski@outlook.com>
2023-01-12 23:15:47 -0500

    Created templates for main components, returning an H1 tag with the component name.

---
+++ src/components/CourseList.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CourseList() {
+    return (
+        <h1>CourseList Component</h1>
+    );
+}
---
+++ src/components/CoursePlan.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function CoursePlan() {
+    return (
+        <h1>CoursePlan Component</h1>
+    );
+}
