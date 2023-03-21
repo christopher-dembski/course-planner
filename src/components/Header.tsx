754142695e4b649536dee0fea55ccc94c34f91fc
christopher-dembski <christopher_dembski@outlook.com>
2023-03-20 22:36:35 -0400

    Added title to Header.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 export default function Header() {
     return (
-        <h1>Header Component</h1>
+        <header className="header p-2" style={{backgroundColor: "rgb(59, 70, 78)"}}>
+            <h1 style={{color: "white"}}>York University - Computer Science Course Planner</h1>
+        </header>
     );
 }
