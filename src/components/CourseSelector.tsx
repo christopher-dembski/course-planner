fc540f9c1d796d8cecd01be6a3c4d6d79245885f
christopher-dembski <christopher_dembski@outlook.com>
2023-03-20 22:40:59 -0400

    Course selector displays all courses and info for selected course.

--- src/components/CourseSelector.tsx
+++ src/components/CourseSelector.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import {CoursesInfo} from '../types';
-
-import placeHolderImage from '../images/course-tree-placeholder-image.jpg';
 
 import './CourseSelector.css';
 
@@ -13,14 +11,48 @@ interface Props {
 }
 
 export default function CourseSelector({addCourse, coursesInfo}: Props) {
+
+    const [courseCodeSelected, setCourseCodeSelected] = useState("EECS-1001");
+    const courseSelected = coursesInfo[courseCodeSelected];
+
+    const years = [1, 2, 3, 4];
+
+    const getCoursesForYear = (year: Number) => {
+        return Object.values(coursesInfo).filter(course => course.year == year);
+    }
+
     return (
-        <section id='course-selector-popup'>
-            <h1>CourseSelector Component</h1>
+        <section id='course-selector-popup' className="d-flex overflow-auto rounded p-3">
             <section>
-                <img src={placeHolderImage} style={{width: '80%'}}></img>
+                {
+                    years.map(year => {
+                        return (
+                            <section className="p-2 mb-5">
+                                <section className="d-flex flex-wrap justify-content-center">
+                                    {
+                                        getCoursesForYear(year).map(course => {
+                                            return (
+                                                <button
+                                                    id="course-box"
+                                                    className="d-flex align-items-center rounded m-2 p-2"
+                                                    onClick={() => setCourseCodeSelected(course.id)}>
+                                                    <p>{course.title}</p>
+                                                </button>
+                                            )
+                                        })
+                                    }
+                                </section>
+                            </section>
+                        );
+                    })
+                }
             </section>
-            {/* EECS-1015 is a placeholder for the course to add */}
-            <button onClick={() => addCourse('EECS-1015')}>Add course</button>
+            <section className="w-75">
+                    <h2>{courseSelected.title}</h2>
+                    <p>{courseSelected.description}</p>
+                <button className="btn btn-primary" type="submit" onClick={() => addCourse('EECS-1015')}>Add Course
+                </button>
+            </section>
         </section>
     );
 }
