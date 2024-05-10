73a999f64383c9fa417c2ce2d3aeeccf85b83dff
christopher-dembski <christopher_dembski@outlook.com>
2024-05-10 18:10:36 -0400

    Filter out invalid courses when loading.

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,33 @@ import "./App.css";
 import defaultPlans from "./data/defaultPlan";
 import coursesData from "./data/coursesData.json";
 
-const loadCoursePlans = () => {
-  const savedPlans = localStorage.getItem("course-plans");
-  return savedPlans ? (JSON.parse(savedPlans) as Array<TPlan>) : defaultPlans;
-};
-
 const loadCoursesData = (): TCoursesData => {
   return Object.freeze(coursesData);
 };
 
+const loadCoursePlans = (coursesData: TCoursesData): TPlan[] => {
+  const savedPlans = localStorage.getItem("course-plans");
+  const plans = savedPlans
+    ? (JSON.parse(savedPlans) as Array<TPlan>)
+    : defaultPlans;
+  return plans.map((plan) => removeInvalidCourses(plan, coursesData));
+};
+
+const removeInvalidCourses = (
+  plan: TPlan,
+  coursesData: TCoursesData,
+): TPlan => {
+  return plan.map((semester) => {
+    return semester.filter((courseCode) => courseCode in coursesData);
+  });
+};
+
 function App() {
+  const coursesData = loadCoursesData();
+
   const [isCourseSelectorDisplayed, setIsCourseSelectorDisplayed] =
     useState(false);
-  const [plans, setPlans] = useState(loadCoursePlans());
+  const [plans, setPlans] = useState(loadCoursePlans(coursesData));
   const [planNumber, setPlanNumber] = useState(0); // index of current course plan to display
   const [termNumber, setTermNumber] = useState(0); // index for term to add course to
 
@@ -55,8 +69,6 @@ function App() {
       return newPlans;
     });
   };
-
-  const coursesData = loadCoursesData();
 
   return (
     <section>
